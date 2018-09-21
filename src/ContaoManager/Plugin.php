04f43f00e8756177f0b23b6a782df515a5cda189
DenizGelion <35569868+DenizGelion@users.noreply.github.com>
2018-09-21 16:07:21 +0200

    Wrong Plugin interface
    Contao\ManagerPlugin\Routing\RoutingPluginInterface
    
    not
    
    Contao\ManagerPlugin\Bundle\Routing\RoutingPluginInterface

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -14,7 +14,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Contao\ManagerPlugin\Bundle\Routing\RoutingPluginInterface;
+use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use Contao\ChallongeBundle\ContaoChallongeBundle;
 use Symfony\Component\Config\Loader\LoaderResolverInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
