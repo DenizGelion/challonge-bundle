513617c0202ab13dad838d97f27975ef202cb507
DenizGelion <35569868+DenizGelion@users.noreply.github.com>
2018-09-24 20:57:17 +0200

    Add files via upload

---
+++ src/ChallongeBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of challonge-bundle.
+ *
+ * (c) Deniz Gelion
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace denizgelion\ChallongeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContaoChallongeBundle extends Bundle
+{
+}
